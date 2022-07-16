fixed normalization of A

lagnet.py
@@ -18,10 +18,10 @@ import pandas as pd
 import os
 
 from models import LagNet
-from utils import construct_S0_S1
+from utils import construct_S
 
 def run(A,X,L,K,d,target,final_activation=None,lam=794e-6,alpha=0.5,seed=1,optim='adam',
-		initial_learning_rate=0.001,beta_1=0.9,beta_2=0.999,epochs=200,
+		initial_learning_rate=0.001,beta_1=0.9,beta_2=0.999,epochs=300,linear=False,
 		save_dir='./results',save_name='lagnet'):
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -35,13 +35,12 @@ def run(A,X,L,K,d,target,final_activation=None,lam=794e-6,alpha=0.5,seed=1,optim
 	A = A.float()
 	X = X.float()
 
-	S_0, _ = construct_S0_S1(A)
+	S = construct_S(A)
 	
-	S_0 = S_0.to(device)
-	#A = A.to(device)
+	S = S.to(device)
 	X = X.to(device)
 
-	model = LagNet(S_0,X,L,K,d,final_activation)
+	model = LagNet(S,X,L,K,d,final_activation)
 	model.to(device)
 
 	criterion = nn.MSELoss()  # reduction = sum?

utils.py
@@ -68,19 +68,15 @@ def dag_orient_edges(adjacency_matrix,pseudotime):
 
 	return D
 
-def construct_S0_S1(D):
-
-	D_0 = D.clone()
-	D_1 = D.clone() + torch.eye(D.shape[0])
-	S_0 = D_0.clone()
-	S_1 = D_1.clone()
-
-	D_0_sum = D_0.sum(1)
-	D_0_sum[D_0_sum == 0] = 1
-	S_0 = (S_0.T/D_0_sum)
-	S_1 = (S_1.T/D_1.sum(1))
-
-	return S_0,S_1
+def construct_S(D):
+    S = D.clone()
+    D_sum = D.sum(0)
+    D_sum[D_sum == 0] = 1
+    
+    S = (S/D_sum)
+    S = S.T
+    
+    return S
 
 def load_multiome_data(data_dir,dataset,sampling=None,preprocess=True):
 